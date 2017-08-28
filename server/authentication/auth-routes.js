e620e7d9ebf66d2ce0f69c5db24907ad1073115e
Tyler Rhinehart <rhinehart22@gmail.com>
2017-08-28 12:28:18 -0600

    changed auth route fron .id to ._id

--- server/authentication/auth-routes.js
+++ server/authentication/auth-routes.js
@@ -4,7 +4,7 @@ let Users = require('../models/user')
 router.post('/register', (req, res) => {
   Users.create(req.body)
     .then((user) => {
-      req.session.uid = user.id
+      req.session.uid = user._id
       req.session.save()
       user.password = null
       delete user.password
